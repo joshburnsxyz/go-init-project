b848d93f4917105e823170cd5c945aec707e7873
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2023-02-07 22:28:29 +0000

    refactoring

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"os/exec"
-
-  "github.com/bitfield/script"
 )
 
 var (
@@ -24,31 +21,13 @@ func main() {
 	flag.Parse()
 
 	// Create project directory
-  if _, err := os.Stat(projectName); os.IsNotExist(err) {
-    os.MkdirAll(projectName, 0644)
-  }
+	if _, err := os.Stat(projectName); os.IsNotExist(err) {
+		os.MkdirAll(projectName, 0777)
+	}
 
 	// Generate project files
 	err := os.Chdir(projectName)
 	if err != nil {
-		fmt.Println("> ERROR: Could not cd into new project directory, wtf?")
-	} else {
-		createMainFileCmd := exec.Command("touch", "main.go")
-		initGoModuleCmd := exec.Command(fmt.Sprintf("go mod init %s", projectName))
-
-		err := createMainFileCmd.Run()
-		if err == nil {
-			script.File("main.go").WriteFile("package main")
-			err := initGoModuleCmd.Run()
-			if err == nil {
-				fmt.Println("done")
-			} else {
-				fmt.Println("> ERROR: Could not initialize go module")
-				os.Exit(2)
-			}
-		} else {
-			fmt.Println("> ERROR: Could not create main.go")
-			os.Exit(2)
-		}
+    // Inside of ./projectName
 	}
 }
